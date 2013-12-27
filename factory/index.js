2ea5a45cdbe6141f44177c44c092d6349db64e22
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-12-27 19:40:28 +0000

    added factory subgenerator

---
+++ factory/index.js
@@ -0,0 +1,17 @@
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+var FactoryGenerator = module.exports = function FactoryGenerator(args, options, config) {
+  // By calling `NamedBase` here, we get the argument to the subgenerator call
+  // as `this.name`.
+  yeoman.generators.NamedBase.apply(this, arguments);
+
+  console.log('You called the factory subgenerator with the argument ' + this.name + '.');
+};
+
+util.inherits(FactoryGenerator, yeoman.generators.NamedBase);
+
+FactoryGenerator.prototype.files = function files() {
+  this.template('template.coffee', 'assets/js/factory/' + _.slugify(this.name) + '-factory.coffee');
+};
