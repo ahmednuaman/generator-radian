983fcf3d91659a1527a78efd673e3973900517c5
Ahmed Nuaman <ahmed@ahmednuaman.com>
2014-01-11 19:21:15 +0000

    let's test

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 var fs = require('fs'),
+    less = require('less'),
     path = require('path'),
+    rimraf = require('rimraf'),
     util = require('util'),
     yeoman = require('yeoman-generator');
 
 module.exports = RadianGenerator;
 util.inherits(RadianGenerator, yeoman.generators.Base);
 
-function RadianGenerator(args, options, config) {
+function RadianGenerator (args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
@@ -133,9 +135,11 @@ RadianGenerator.prototype.askFor = function () {
   });
 };
 
-RadianGenerator.prototype.app = function app() {
+RadianGenerator.prototype.app = function () {
   var done = this.async(),
-      extCSS = (this.precompilerCSS || 'sass');
+      extCSS = this.precompilerCSS || 'less',
+      that = this,
+      css;
 
   this.template('_bower.json', 'bower.json');
   this.template('_package.json', 'package.json');
@@ -143,6 +147,10 @@ RadianGenerator.prototype.app = function app() {
   this.template('_radianrc', '.radianrc');
 
   this.remote('ahmednuaman', 'radian', 'v0.7.0', function (err, remote) {
+    if (err) {
+      done(err);
+    }
+
     remote.copy('.bowerrc', '.bowerrc');
     remote.copy('.editorconfig', '.editorconfig');
     remote.copy('.gitignore', '.gitignore');
@@ -152,7 +160,7 @@ RadianGenerator.prototype.app = function app() {
 
     remote.directory('grunt', 'grunt');
 
-    if (!this.includeExample) {
+    if (!that.includeExample) {
       remote.copy('assets/coffee/app.coffee', 'assets/coffee/app.coffee');
       remote.copy('assets/coffee/partials.coffee', 'assets/coffee/partials.coffee');
       remote.copy('assets/coffee/startup.coffee', 'assets/coffee/startup.coffee');
@@ -160,21 +168,21 @@ RadianGenerator.prototype.app = function app() {
       remote.copy('test/unit/karma.conf.coffee', 'test/unit/karma.conf.coffee');
       remote.copy('test/unit/test-main.coffee', 'test/unit/test-main.coffee');
 
-      this.mkdir('assets/' + extCSS + '/partial');
-      this.mkdir('assets/img');
-      this.mkdir('assets/coffee/collection');
-      this.mkdir('assets/coffee/controller');
-      this.mkdir('assets/coffee/directive');
-      this.mkdir('assets/coffee/factory');
-      this.mkdir('assets/coffee/service');
-      this.mkdir('assets/coffee/vo');
-      this.mkdir('assets/partial');
-      this.mkdir('test/unit/collection');
-      this.mkdir('test/unit/controller');
-      this.mkdir('test/unit/directive');
-      this.mkdir('test/unit/factory');
-      this.mkdir('test/unit/service');
-      this.mkdir('test/unit/vo');
+      that.mkdir('assets/' + extCSS + '/partial');
+      that.mkdir('assets/img');
+      that.mkdir('assets/coffee/collection');
+      that.mkdir('assets/coffee/controller');
+      that.mkdir('assets/coffee/directive');
+      that.mkdir('assets/coffee/factory');
+      that.mkdir('assets/coffee/service');
+      that.mkdir('assets/coffee/vo');
+      that.mkdir('assets/partial');
+      that.mkdir('test/unit/collection');
+      that.mkdir('test/unit/controller');
+      that.mkdir('test/unit/directive');
+      that.mkdir('test/unit/factory');
+      that.mkdir('test/unit/service');
+      that.mkdir('test/unit/vo');
 
       remote.copy('assets/coffee/controller/radian-controller.coffee', 'assets/coffee/controller/radian-controller.coffee');
       remote.copy('assets/coffee/directive/radian-directive.coffee', 'assets/coffee/directive/radian-directive.coffee');
@@ -183,7 +191,7 @@ RadianGenerator.prototype.app = function app() {
       remote.copy('assets/coffee/service/radian-service.coffee', 'assets/coffee/service/radian-service.coffee');
       remote.copy('assets/coffee/helper/radian-module-helper.coffee', 'assets/coffee/helper/radian-module-helper.coffee');
 
-      if (this.includeStubs) {
+      if (that.includeStubs) {
         remote.copy('assets/coffee/collection/stub-collection.coffee', 'assets/coffee/collection/stub-collection.coffee');
         remote.copy('assets/coffee/controller/stub-controller.coffee', 'assets/coffee/controller/stub-controller.coffee');
         remote.copy('assets/coffee/directive/stub-directive.coffee', 'assets/coffee/directive/stub-directive.coffee');
@@ -193,25 +201,35 @@ RadianGenerator.prototype.app = function app() {
         remote.copy('assets/partial/directive/stub-partial.jade', 'assets/partial/directive/stub-partial.jade');
       }
 
-      this.template('assets/' + extCSS + '/styles.' + extCSS, 'assets/' + extCSS + '/styles.' + extCSS);
-      this.template('assets/' + extCSS + '/_partials.' + extCSS, 'assets/' + extCSS + '/_partials.' + extCSS);
-      this.template('assets/js/config.coffee', 'assets/coffee/config.coffee');
-      this.template('assets/js/routes.coffee', 'assets/coffee/routes.coffee');
-      this.template('assets/js/controller/app-controller.coffee', 'assets/coffee/controller/app-controller.coffee');
+      that.template('assets/' + extCSS + '/styles.' + extCSS, 'assets/' + extCSS + '/styles.' + extCSS);
+      that.template('assets/' + extCSS + '/_partials.' + extCSS, 'assets/' + extCSS + '/_partials.' + extCSS);
+      that.template('assets/coffee/config.coffee', 'assets/coffee/config.coffee');
+      that.template('assets/coffee/routes.coffee', 'assets/coffee/routes.coffee');
+      that.template('assets/coffee/controller/app-controller.coffee', 'assets/coffee/controller/app-controller.coffee');
     } else {
       remote.directory('assets/img', 'assets/img');
-      remote.directory('assets/js', 'assets/js');
+      remote.directory('assets/coffee', 'assets/coffee');
       remote.directory('assets/partial', 'assets/partial');
       remote.directory('data', 'data');
       remote.directory('test', 'test');
 
-      remote.copy('assets/css/**/*.' + extCSS, 'assets/css');
-
-      if (!this.precompilerCSS) {
-
-      }
+      remote.copy('assets/' + extCSS, 'assets/' + extCSS);
     }
 
-    done();
-  }.bind(this));
+    if (!that.precompilerCSS) {
+      less.render(that.readFileAsString(path.join(__dirname, 'assets/less/styles.less')), function (err, css) {
+        if (err) {
+          done(err);
+        }
+
+        that.write('assets/css/styles.css', css);
+
+        rimraf('assets/' + extCSS, function (err) {
+          done(err);
+        });
+      });
+    } else {
+      done();
+    }
+  });
 };
